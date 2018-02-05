c28156b3dfc85e5a3a971c617a974273bbe1ab7e
heqian <1187984211@qq.com>
2018-02-05 13:07:44 +0800

    update stack

---
+++ stack.go
@@ -0,0 +1,35 @@
+package stack
+
+import "errors"
+
+//the size of the stack is allocated based on the slice
+type Stack struct {
+	data []interface{}
+}
+
+// Push adds x to the top of the stack.
+func (s *Stack) Push(x interface{}) {
+	s.data = append(s.data, x)
+}
+
+// Pop removes and returns the top element of the stack.
+// It's a run-time error to call Pop on an empty stack.
+func (s *Stack) Pop()( interface{}, error){
+	if s.Size() == 0{
+		return nil, errors.New("stack is empty")
+	}
+	i := len(s.data) - 1
+	res := s.data[i]
+	s.data[i] = nil // to avoid memory leak
+	s.data = s.data[:i]
+	return res,nil
+}
+
+// Size returns the number of elements in the stack.
+func (s *Stack) Size() int {
+	return len(s.data)
+}
+
+func (s *Stack)IsEmpty() bool {
+	return len(s.data) == 0
+}
