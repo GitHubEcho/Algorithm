2cb7210bcae7b10988bb5ba55019553ac1eb4cd8
heqian <1187984211@qq.com>
2018-02-17 23:32:51 +0800

    update code10

--- 剑指offer（Golang实现）/code10.go
+++ 剑指offer（Golang实现）/code10.go
@@ -1,36 +1,59 @@
 /*
  *Author: skyler
- *Date: 2018-02-16
+ *Date: 2018-02-17
  */
 
-/*跳台阶
+/*二进制中1的个数
  *描述：
- *  一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ *  输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ *相关：
+ *  所用知识点：移位运算和与运算。这些运算往往会给我们带来便利性。
+ *  &运算后返回值为0或1，移位运算3（00000011）右移为1（00000001）。
  *分析：
- *  当要跳n次时用f(n)表示，最后一跳有两种方式跳1级f(n-1)和跳2级f(n-2)，f(n)=f(n-1)+f(n-2)，即斐波那契数列。
+ *  ①每次原数n向右移动然后做与运算，返回值为int类型1和0，当为1时count计数加1
+ *  ②在①的基础上，避免输入n的数字为负数时，发生错误。用左移1与原数字做与运算。
+ *  ③此种方法充分利用二进制的特性，把数n减去1和n做与运算，会把n的最右边少一个1变为0，循环做此种运算直到n的值为0。
  */
-
 package main
 
 import "fmt"
 
-const LIM = 100
-
-var fibs [LIM]uint
-
-func Fib(n int) (res uint) {
-	if fibs[n] != 0 {
-		res = fibs[n]
+func NumberOfOne1(n int) int {
+	count := 0
+	for n != 0 { // bool(n)编译错误，go语言中1不表示ture。
+		if (n & 1) != 0 {
+			count ++
+		}
+		n = n >> 1
 	}
-	if n <= 1 {
-		return uint(n)
-	} else {
-		res = Fib(n-1) + Fib(n-2)
-	}
-	fibs[n] = res
-	return
+	return count
 }
 
+func NumberOfOne2(n int) int {
+	flag := 1
+	count := 0
+	for flag != 0 {
+		if (n & flag) != 0 {
+			count ++
+		}
+		flag = flag << 1
+	}
+	return count
+}
+
+func NumberOfOne3(n int) int {
+	count := 0
+	for n != 0 {
+		count ++
+		n = (n - 1) & n
+
+	}
+	return count
+}
+
+
 func main() {
-	fmt.Println(Fib(10))
+	fmt.Println(NumberOfOne1(3))
+	fmt.Println(NumberOfOne2(3))
+	fmt.Println(NumberOfOne3(3))
 }
